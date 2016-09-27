332ed963c3975c193d1f7eb18d13fb0764a03d0f
bhanu475 <bhanuharsha475@gmail.com>
2016-09-27 23:39:54 +0530

    Initial

---
+++ go/products.go
@@ -0,0 +1,15 @@
+package 
+
+import (
+	"net/http"
+)
+
+type Products struct {
+
+}
+
+func ProductsGet(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Content-Type", "application/json; charset=UTF-8")
+		w.WriteHeader(http.StatusOK)
+}
+
